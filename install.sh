4681c180c7fbfbf501b99287888fa6509695480a
Murugespandian S <murugespandian.s@zohocorp.com>
2016-05-25 17:14:03 +0530

    Initial file checkin

---
+++ install.sh
@@ -0,0 +1,18 @@
+
+mkdir ~/.RM-Trash
+DIR=$PWD
+
+echo "
+if [ -f $PWD/bash/rmbash ]; then
+	source $PWD/bash/rmbash 
+fi " >> ~/.bash_profile
+
+
+crontab -l | { cat; echo "0 0 * * * rmf -rvf /Users/muruges-0043/.RM-Trash/*"; } | crontab -
+
+
+source ~/.bash_profile
+
+echo " INSTALLED SUCCESSFULLY"
+
+echo "***HELP*** \n 1.rm filename,directory or wildcard(*) , --to trash the file to the RM-Trash directory(Automatic file cleaning process will take place at every day 12AM) \n 2.rmf filename or directory, --to permenently remove the file or directory \n 3. undo --to undo the last removed action"
